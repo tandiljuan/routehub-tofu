564fb3270f6c68f092b9562acba7bf725905892d
Juan Manuel Lopez <git.nrbye@slmail.me>
2026-05-23 21:09:06 -0300

    feat: Implement dynamic firewall creation via variable

--- network.tf
+++ network.tf
@@ -42,3 +42,30 @@ resource "hcloud_firewall" "ssh" {
   }
   labels = merge(local.common_labels, { role = "ssh" })
 }
+
+resource "hcloud_firewall" "dynamic" {
+  for_each = var.htz_fwl_lst
+  name = "${var.htz_pfx}-${each.value.name}"
+  dynamic "rule" {
+    for_each = each.value.rule
+    content {
+      direction = rule.value.direction
+      protocol = rule.value.protocol
+      port = rule.value.port
+      source_ips = coalesce(rule.value.source, [])
+      destination_ips = coalesce(rule.value.destination, [])
+    }
+  }
+  dynamic "apply_to" {
+    for_each = (
+      can(keys(each.value.to_label))
+      ? [for k, v in each.value.to_label: "${k}=${v}"]
+      : try(compact(each.value.to_label), split(",", each.value.to_label), [])
+    )
+    iterator = label
+    content {
+      label_selector = label.value
+    }
+  }
+  labels = merge(local.common_labels, { role = "dynamic" })
+}
